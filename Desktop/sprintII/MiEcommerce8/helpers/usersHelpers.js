681baec923607e705918b7aa6d13633b8510046a
MRNCallero <mariano.callero@cencosud.com>
2022-10-03 10:36:36 -0300

    cambios en demo

--- Desktop/sprintII/MiEcommerce8/helpers/usersHelpers.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-let usersHelpers = {
-    readBaseUsers : ()=>JSON.parse(fs.readFileSync(path.join(__dirname, "../api/data/users.json"),"utf8")),
-    writeBaseUsers : (u)=>fs.writeFileSync(path.join(__dirname, "/../api/data/users.json"),JSON.stringify(u))
-}
-
-module.exports = usersHelpers;
